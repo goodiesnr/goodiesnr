36d09dae97c9cae50a1fc12c3516319ed8f43244
goodie77codes <shikamaru1082@gmail.com>
2026-07-27 18:48:56 -0700

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,19 @@ export default function App() {
       description: "A QRcode generator tool.",
     },
     {
+      title: "Gr8reviews",
+      domain: "GITHUB.COM/GOODIESNR/GR8REVIEW_BACKEND",
+      link: "https://github.com/goodiesnr/gr8review_backend",
+      description: "A lightweight rating/review platform.",
+    },
+    {
+      title: "Aplus Painting",
+      domain: "APLUSPAINTING.VERCEL.APP",
+      link: "https://apluspainting.vercel.app",
+      description:
+        "A simple brochure website for a painting company.",
+    },
+    {
       title: "Tic-Tac-Toe AI",
       domain: "GOODIESNR.VERCEL.APP",
       link: "https://goodiesnr.vercel.app/playground",
